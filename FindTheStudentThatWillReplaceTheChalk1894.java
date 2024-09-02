27b18824b98135084f50e499cd2bdfe783ecdf21
tanXnyx <tanmay766745@gmail.com>
2024-09-02 23:15:50 +0530

    Q 1894  day 43

---
+++ FindTheStudentThatWillReplaceTheChalk1894.java
@@ -0,0 +1,19 @@
+public class FindTheStudentThatWillReplaceTheChalk1894 {
+    public int chalkReplacer(int[] chalk, int initialChalkPieces) {
+        long totalChalkNeeded = 0;
+        for (int studentChalkUse : chalk) {
+            totalChalkNeeded += studentChalkUse;
+        }
+        
+        int remainingChalk = (int)(initialChalkPieces % totalChalkNeeded);
+        
+        for (int studentIndex = 0; studentIndex < chalk.length; studentIndex++) {
+            if (remainingChalk < chalk[studentIndex]) {
+                return studentIndex;
+            }
+            remainingChalk -= chalk[studentIndex];
+        }
+        
+        return 0;  
+    }
+}

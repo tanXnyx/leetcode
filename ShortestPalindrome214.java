95724fa8fed45aaf6fdd32bbe8d707e6b63206bc
tanXnyx <tanmay766745@gmail.com>
2024-09-20 23:27:07 +0530

    Q214

---
+++ ShortestPalindrome214.java
@@ -0,0 +1,51 @@
+public class ShortestPalindrome214 {
+    /*
+     * You are given a string s. You can convert s to a 
+palindrome
+ by adding characters in front of it.
+
+Return the shortest palindrome you can find by performing this transformation.
+
+ 
+
+Example 1:
+
+Input: s = "aacecaaa"
+Output: "aaacecaaa"
+Example 2:
+
+Input: s = "abcd"
+Output: "dcbabcd"
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of lowercase English letters only.
+     */
+    public String shortestPalindrome(String s) {
+        int count = kmp(new StringBuilder(s).reverse().toString(), s);
+        return new StringBuilder(s.substring(count)).reverse().toString() + s;
+    }
+
+    private int kmp(String txt, String patt) {
+        String newString = patt + '#' + txt;
+        int[] pi = new int[newString.length()];
+        int i = 1, k = 0;
+        while (i < newString.length()) {
+            if (newString.charAt(i) == newString.charAt(k)) {
+                k++;
+                pi[i] = k;
+                i++;
+            } else {
+                if (k > 0) {
+                    k = pi[k - 1];
+                } else {
+                    pi[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return pi[newString.length() - 1];
+    } 
+}

859a2643bc43e7960f1193f97ccc332e74e089fc
tanmay-Kumar-Swain <129646121+tanXnyx@users.noreply.github.com>
2024-10-24 22:02:50 +0530

    Add files via upload

---
+++ FlipEquivalentBinaryTrees951.java
@@ -0,0 +1,62 @@
+import javax.swing.tree.TreeNode;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ * For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.
+
+ 
+
+Example 1:
+
+Flipped Trees Diagram
+Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+Output: true
+Explanation: We flipped at nodes with values 1, 3, and 5.
+Example 2:
+
+Input: root1 = [], root2 = []
+Output: true
+Example 3:
+
+Input: root1 = [], root2 = [1]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in each tree is in the range [0, 100].
+Each tree will have unique node values in the range [0, 99].
+ */
+public class FlipEquivalentBinaryTrees951 {
+       public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        
+        return checker(root1, root2);
+    }
+
+    private boolean checker(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null || node1.val != node2.val) {
+            return false;
+        }
+
+        return (checker(node1.left, node2.left) || checker(node1.left, node2.right)) &&
+               (checker(node1.right, node2.right) || checker(node1.right, node2.left));
+    }
+}
